b5f6fccd6827796efc7aa96b939efafc50564e47
stian.overby <stian.overby@nav.no>
2023-01-20 11:00:21 +0100

    Now shows all available contexts and applications

--- getenv.sh
+++ getenv.sh
@@ -2,7 +2,7 @@
 #
 # Author: Stian Øverby
 #
-# This script is used to retrive AZURE_APP_JWK and AZURE_APP_CLIENT_SECRET from the pods of a wanted 
+# This script is used to retrive environment variables from the pods of a wanted 
 # context in a kubernetes cluster. 
 # 
 #
@@ -21,7 +21,7 @@ contexts=$(kubectl config get-contexts | awk 'NR > 1 {print $2}')
 
 printf "\n== START CONTEXT NAMES ==\n\n"
 echo "$contexts" 
-printf "\n== END CONTEXT NAMES ==\n"
+printf "\n== END CONTEXT NAMES ==\n\n"
 
 # Prompt the user for the context
 read -p "Enter the wanted context => " context
@@ -49,7 +49,7 @@ applications=$(kubectl get application | awk 'NR > 1 {print $1}')
 
 printf "\n== START APPLICATION NAMES ==\n\n"
 echo "$applications"
-printf "\n== END APPLICATION NAMES ==\n"
+printf "\n== END APPLICATION NAMES ==\n\n"
 
 # Prompt the user for the application
 read -p "Enter the name of the application => " application
@@ -57,14 +57,23 @@ read -p "Enter the name of the application => " application
 # Get the name of the first pod with the specified application
 pod_name=$(kubectl get pods --field-selector=status.phase=Running -l app=$application -o jsonpath='{.items[0].metadata.name}')
 
+#########################################################
+#          GET VALUES OF ENVIRONMENT VARIABLES          #
+#########################################################
 # Get the value of the AZURE_APP_JWK environment variable
 azure_app_jwk=$(kubectl exec $pod_name -- env | grep ^AZURE_APP_JWK= )
 
 # Get the value of the AZURE_APP_CLIENT_SECRET environment variable
 azure_app_client_secret=$(kubectl exec $pod_name -- env | grep AZURE_APP_CLIENT_SECRET)
 
+# Get the values of all the environment variables that is an AAD_CLIENT
+aad_clients=$(kubectl exec $pod_name -- env | grep AAD_CLIENT)
+
+# Print env vars to stdout
 printf "\n=== START ENV VARS ===\n\n"
 echo "$azure_app_jwk"
 printf "\n"
 echo "$azure_app_client_secret"
+printf "\n"
+echo "$aad_clients"
 printf "\n=== END ENV VARS ===\n"
