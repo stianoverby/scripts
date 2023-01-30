84f88461ef9a9b3c6ddb5e9520aa57e0b9adef8e
stian.overby <stian.overby@nav.no>
2023-01-30 15:09:52 +0100

    refactor

--- match.py
+++ match.py
@@ -1,14 +1,15 @@
-'''
+"""
     DIVIDE PARTICIPANTS INTO ALLOWED/NOT ALLOWED AGE GROUPS
         Script developed to help an voluntary organization to filter what participants 
         are allowed to take part of an event. Allowed participants are those who are of
         the allowed age during the entire event.
-'''
+"""
 
 from datetime import datetime
 import csv
 
-date_format = '%d.%m.%Y'
+date_format = "%d.%m.%Y"
+
 
 def read_events(filename):
     # Create an empty list to store the data
@@ -24,12 +25,13 @@ def read_events(filename):
 
     return data
 
+
 def extract_participants(filename):
-    
+
     with open(filename) as file:
         csvreader = csv.reader(file)
 
-        #Extract the headers
+        # Extract the headers
         header = []
         iterator = csvreader
         header = next(iterator)
@@ -44,32 +46,41 @@ def extract_participants(filename):
     combined = combine_rows_and_header(rows, header)
 
     # Extract only the columnst with the wanted headers
-    participants = [{x : y for x,y in m if is_wanted_column((x,y))} for m in combined]
-    
+    participants = [{x: y for x, y in m if is_wanted_column((x, y))} for m in combined]
+
     # Filter out duplicates of participants
     unique_participants = []
     for idx, participant in enumerate(participants):
-        if participant not in participants[idx+1 : ]:
+        if participant not in participants[idx + 1 :]:
             unique_participants.append(participant)
-    
+
     return unique_participants
+
 
 def is_wanted_column(tpl):
     wanted_column_headers = ["Fornavn", "Etternavn", "Fødselsdato", "Kjønn"]
     header, _ = tpl
     return header in wanted_column_headers
 
-''' Date helper functions '''
+
 def convert_to_date(date):
     return datetime.strptime(date, date_format).date()
 
+
 def after(d1, d2):
-    return d2.month <= d1.month and d2.day < d1.day
+    if d2.month < d1.month:
+        return True
+    return d2.month == d1.month and d2.day < d1.day
+
 
 def before(d1, d2):
-    return d1.month <= d2.month and d1.day <= d2.day 
+    # The before function is not as strict, since if you have
+    # your birthday the first day of the event you are allowed to go
+    if d1.month < d2.month:
+        return True
+    return d1.month == d2.month and d1.day <= d2.day
 
-''' Functions for moving around lists '''
+
 def combine_rows_and_header(nested_lst, flat_lst):
     result = []
     for lst in nested_lst:
@@ -79,79 +90,104 @@ def combine_rows_and_header(nested_lst, flat_lst):
         result.append(new_lst)
     return result
 
-def filter_on_age(kids, legal_age, start_date, end_date, negate_predicate = False):
-    
-    # Filter function
+
+def partition_participants(participants, start_date, end_date):
+
+    legal_ages = [16, 17]
+
+    """ Predicate for filtering """
+
     def allowed_age(kid):
-        birthday = datetime.strptime(kid['Fødselsdato'], "%d.%m.%Y")
+        birthday = datetime.strptime(kid["Fødselsdato"], "%d.%m.%Y")
         age_this_year = end_date.year - birthday.year
 
         # Becoming the minimum required age and has birthday before the start date of the event
-        if age_this_year == min(legal_age) and before(birthday, start_date):
+        if age_this_year == min(legal_ages) and before(birthday, start_date):
             return True
-        
+
         # Becoming an age in the allowed range that is not the minimum
-        if 1 < len(legal_age) and age_this_year in legal_age[1:]:
-            return True 
+        if 1 < len(legal_ages) and age_this_year in legal_ages[1:]:
+            return True
 
         # Becoming older than the maximum legal age, but has birthday after the end of the event
-        if age_this_year == max(legal_age) + 1 and after(birthday, end_date):
-            return True 
+        if age_this_year == max(legal_ages) + 1 and after(birthday, end_date):
+            return True
 
         return False
 
-    if negate_predicate:
-        return list(filter(lambda x: not allowed_age(x), kids))
+    allowed = [p for p in participants if allowed_age(p)]
+    not_allowed = [p for p in participants if not allowed_age(p)]
 
-    return list(filter(allowed_age, kids))
+    return allowed, not_allowed
 
-def process_participants(participants, event, legal_age):
-    
+
+def process_participants(participants, event):
+
     event_name, sdate, edate = event
 
     start_date = datetime.strptime(sdate, date_format)
     end_date = datetime.strptime(edate, date_format)
 
-    legal_participants = filter_on_age(participants, legal_age, start_date, end_date)
-    possible_participants = ([list(dict.values()) for dict in legal_participants])
-    possible_participants.sort()
-    
-    illegal_participants = filter_on_age(participants, legal_age, start_date, end_date, negate_predicate=True)
-    not_possible_participants = ([list(dict.values()) for dict in illegal_participants])
-    not_possible_participants.sort()
+    allowed_participants, not_allowed_participants = partition_participants(
+        participants, start_date, end_date
+    )
 
-    output = ""
-    output += "\nEVENT => " + event_name.upper() + "\n"
-    output += "=====================================================\n"
-    output += pretty_str("   ALLOWED TO GO\n", possible_participants)
-    output += pretty_str("\n   TOO YOUNG OR OLD\n", not_possible_participants)
-    output += "=====================================================\n"
+    allowed_participants = [list(dict.values()) for dict in allowed_participants]
+    allowed_participants.sort()
+    not_allowed_participants = [
+        list(dict.values()) for dict in not_allowed_participants
+    ]
+    not_allowed_participants.sort()
+
+    output = pretty_output_event(
+        event_name, allowed_participants, not_allowed_participants
+    )
 
     return output
 
-def pretty_str(title, participants):
+
+def pretty_output_event(event_name, allowed_participants, not_allowed_participants):
+    header_event = "\nEVENT => " + event_name.upper() + "\n"
+    header_allowed = "   ALLOWED TO GO\n"
+    header_not_allowed = "\n   TOO YOUNG OR OLD\n"
+    header_bar = "=====================================================\n"
+
+    output = header_event
+    output += header_bar
+    output += pretty_output_participants(header_allowed, allowed_participants)
+    output += pretty_output_participants(header_not_allowed, not_allowed_participants)
+    output += header_bar
+    return output
+
+
+def pretty_output_participants(title, participants):
+    header_women = "   |Kvinner|\n"
+    header_men = "\n   |Menn|\n"
+    header_n_participants = (
+        "\n   Participants in category : " + str(len(participants)) + "\n"
+    )
+
     output = title
-    output += "   |Kvinner|\n"
+    output += header_women
     for participant in participants:
         if participant[3] == "Kvinne":
             output += "   " + str(participant) + "\n"
 
-    output += "\n   |Menn|\n"
+    output += header_men
     for participant in participants:
         if participant[3] == "Mann":
             output += "   " + str(participant) + "\n"
-    output += "\n   Participants in category : " + str(len(participants)) + "\n"
+    output += header_n_participants
     return output
+
 
 if __name__ == "__main__":
     participants = extract_participants("input.csv")
     events = read_events("events.txt")
-    legal_age = [16, 17]
 
     output = ""
     for event in events:
-        output += process_participants(participants, event, legal_age)
-    
+        output += process_participants(participants, event)
+
     with open("output.txt", "w") as file:
         file.write(output)
-
